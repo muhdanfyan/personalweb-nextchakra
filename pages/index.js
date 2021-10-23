376f29ca6ffc9f637d8c28d615e506fd56e0495b
Muhdan Fyan Syah Sofian <muhdanfyan@gmail.com>
2021-10-23 16:16:52 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import DarkModeSwitch from '../components/DarkModeSwitch'
+import Container from '../components/Container'
 
 export default function Home() {
   return (
-    <>
-      Index
-    </>
+    <Container>
+    </Container>
   )
 }
